dba7e1d7b7be46517922d07fe175af17bef4597e
mike72312 <mjansbro@gmail.com>
2025-06-01 21:05:27 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import MyTeamPage from './pages/MyTeamPage';
+import AvailableWrestlersPage from './pages/AvailableWrestlersPage';
+import StandingsPage from './pages/StandingsPage';
+import LoginPage from './pages/LoginPage';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <h1>Fantasy Wrestling League</h1>;
+  const teamName = localStorage.getItem('teamName');
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={teamName ? <Navigate to="/my-team" /> : <LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/my-team" element={<MyTeamPage />} />
+        <Route path="/available" element={<AvailableWrestlersPage />} />
+        <Route path="/standings" element={<StandingsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
