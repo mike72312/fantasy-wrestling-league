3081a51c92b0f5e177ed8d44f68d4499729ab116
Mike A <mjansbro@gmail.com>
2025-06-01 20:44:56 -0400

    Push v3.2 with login and nav fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import MyTeamPage from './pages/MyTeamPage';
-import AvailablePage from './pages/AvailablePage';
-import StandingsPage from './pages/StandingsPage';
-import LoginPage from './pages/LoginPage';
-import Navbar from './components/Navbar';
+import React from 'react';
 
 function App() {
-  const [teamName, setTeamName] = useState(localStorage.getItem('teamName'));
-
-  useEffect(() => {
-    const stored = localStorage.getItem('teamName');
-    if (!stored) {
-      setTeamName(null);
-    }
-  }, []);
-
-  return (
-    <Router>
-      {teamName ? (
-        <>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Navigate to="/my-team" />} />
-            <Route path="/my-team" element={<MyTeamPage teamName={teamName} />} />
-            <Route path="/available" element={<AvailablePage teamName={teamName} />} />
-            <Route path="/standings" element={<StandingsPage />} />
-          </Routes>
-        </>
-      ) : (
-        <Routes>
-          <Route path="*" element={<LoginPage onLogin={(name) => {
-            localStorage.setItem('teamName', name.toLowerCase());
-            setTeamName(name.toLowerCase());
-          }} />} />
-        </Routes>
-      )}
-    </Router>
-  );
+  return <h1>Fantasy Wrestling League</h1>;
 }
 
 export default App;
