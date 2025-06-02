9416d74b656a8afac73c4c7470790f17f3a3e8ef
Mike A <mjansbro@gmail.com>
2025-06-01 20:30:02 -0400

    Fix allowedHosts setting for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
+  preview: {
     port: 4173,
-    strictPort: true,
-    preview: {
-      port: 4173,
-      allowedHosts: ['wrestling-frontend.onrender.com'],
-    },
+    host: true,
+    allowedHosts: ['wrestling-frontend.onrender.com'],
   },
 });
+
